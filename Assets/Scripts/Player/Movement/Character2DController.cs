ac00031f48a1791bb4aa18915f1027ecf77e2753
SylvainC <sylvain.capart@hotmail.fr>
2019-03-25 16:02:49 +0100

    commit_0001
    - scene beginning
    - assets 2D import
    - player : playermovement and charactercontroller
    - animations created
    - idle animation working in scene

---
+++ Assets/Scripts/Player/Movement/Character2DController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Character2DController : MonoBehaviour
+{
+    private Rigidbody2D m_PlayerRigidbody;
+    private Vector2 m_RefPlayerVelocity;
+    [SerializeField] [Range(0f, 1f)] private float m_PlayerMovemementSmoothing = 0.1f;
+    [SerializeField] private float m_PlayerSpeed = 70f;
+
+    private bool m_FacingRight = true;
+
+    private void Start()
+    {
+        m_PlayerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(float _PlayerHorizontalMove, float _PlayerVerticalMove, bool _PlayerJump, bool _PlayerRun)
+    {
+        Vector2 targetSpeed = new Vector2(_PlayerHorizontalMove * m_PlayerSpeed, _PlayerVerticalMove * m_PlayerSpeed);
+
+        m_PlayerRigidbody.velocity = Vector2.SmoothDamp(m_PlayerRigidbody.velocity, targetSpeed, ref m_RefPlayerVelocity, m_PlayerMovemementSmoothing);
+
+        if (m_FacingRight && _PlayerHorizontalMove < 0f)
+        {
+            FlipScale();
+        }
+        else if (!m_FacingRight && _PlayerHorizontalMove > 0f)
+        {
+            FlipScale();
+        }
+    }
+
+    private void FlipScale()
+    {
+        m_FacingRight = !m_FacingRight;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+
+    }
+
+}
---
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Character2DController m_PlayerController;
+
+    private float m_PlayerHorizontalMove = 0f;
+    private float m_PlayerVerticalMove = 0f;
+    private bool m_PlayerJump = false;
+    private bool m_PlayerRun = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_PlayerHorizontalMove = Input.GetAxisRaw("Horizontal");
+        m_PlayerVerticalMove = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            m_PlayerJump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        m_PlayerController.Move(m_PlayerHorizontalMove * Time.fixedDeltaTime, m_PlayerVerticalMove * Time.fixedDeltaTime, m_PlayerJump, m_PlayerRun);
+
+        m_PlayerJump = false;
+    }
+}
